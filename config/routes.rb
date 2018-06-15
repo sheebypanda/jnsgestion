88b4e857db9d090459e309d568ec1b0a5212de53
sheebypanda <sheebypanda@gmail.com>
2018-06-15 11:30:03 +0200

    split client fournisseur

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'contacts#index'
+
+  get '/clients' to: 'contacts#clients'
+  get '/fournisseur' to: 'contacts#fournisseurs'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
